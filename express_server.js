4cc19f05283ed4eaf58559ae940b579995ba195e
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-14 04:45:40 +0000

    added permission functions

--- express_server.js
+++ express_server.js
@@ -34,10 +34,26 @@ const getUserIdFromEmail = function(email, database) {
   }
 };
 
+const urlsForUser = (id, database) => {
+  let userUrls = {};
+  for (const shortUrl in database) {
+    if(database[shortUrl].userID === id) {
+      userUrls[shortUrl] = database[shortUrl];
+    }
+  }
+  return userUrls;
+};
+
 // Sample Databases
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "userRandomID",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "userRandomID",
+  },
 };
 
 const users = {
@@ -58,16 +74,24 @@ app.get("/", (req, res) => {
   res.redirect("/urls");
 });
 
-//
+// Redirects to the longURL associated with the tinyURL
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
+  const longURL = urlDatabase[req.params.id].longURL;
+  if (!longURL) {
+    res.status
+  } else {
+    res.redirect(longURL);
+  }
 });
 
 // Register page
 app.get('/register', (req, res) => {
   const templateVars = { user: users[req.cookies.user_id]};
-  res.render("urls_register", templateVars);
+  if (users[req.cookies.user_id] === undefined) {
+    res.redirect('/urls')
+  } else {
+    res.render("urls_register", templateVars);
+  }
 });
 
 app.post('/register', (req, res) => {
@@ -93,7 +117,11 @@ app.post('/register', (req, res) => {
 // Login
 app.get('/login', (req, res) => {
   const templateVars = { user: users[req.cookies.user_id]};
-  res.render('urls_login', templateVars);
+  if (users[req.cookies.user_id] === undefined) {
+    res.render('urls_login', templateVars);
+  } else {
+    res.redirect('/urls');
+  }
 });
 
 app.post('/login', (req, res) => {
@@ -119,14 +147,33 @@ app.post('/logout', (req, res) => {
 
 // URLs
 app.get("/urls", (req, res) => {
-  const templateVars = { user: users[req.cookies.user_id], urls: urlDatabase };
+  let templateVars = {};
+  if (users[req.cookies.user_id] === undefined) {
+    templateVars = { 
+      user: users[req.cookies.user_id],
+      urls: urlsForUser(users[req.cookies.user_id], urlDatabase) 
+    };
+  } else {
+    templateVars = { 
+      user: users[req.cookies.user_id],
+      urls: urlsForUser(users[req.cookies.user_id].id, urlDatabase) 
+    }
+  }
+  console.log(users[req.cookies.user_id]);
   res.render("urls_index", templateVars);
 });
 
 app.post('/urls', (req, res) => {
   const tinyUrl = generateRandomString();
-  urlDatabase[tinyUrl] = req.body.longURL;
-  res.redirect('urls/' + tinyUrl);
+  if (users[req.cookies.user_id] === undefined) {
+    res.status(403).send("Please log in to use this feature")
+  } else {
+    urlDatabase[tinyUrl] = {
+      longURL:req.body.longURL,
+      userID: users[req.cookies.user_id].id
+    };
+    res.redirect('urls/' + tinyUrl);
+  }
 });
 
 // Add URL
@@ -137,19 +184,30 @@ app.get("/urls/new", (req, res) => {
 
 //
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { user: users[req.cookies.user_id], id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { user: users[req.cookies.user_id], id: req.params.id, longURL: urlDatabase[req.params.id].longURL };
   res.render("urls_show", templateVars);
 });
 
 // Delete URL
 app.post('/urls/:id/delete', (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect('/urls');
+  if (users[req.cookies.user_id] === undefined) {
+    res.status(403).send("Please log in to use this feature")
+  } else {
+    delete urlDatabase[req.params.id];
+  }
+    res.redirect('/urls');
 });
 
 // Edit URL
 app.post('/urls/:id/edit', (req, res) => {
-  urlDatabase[req.params.id] = req.body.newURL;
+  if (users[req.cookies.user_id] === undefined) {
+    res.status(403).send("Please log in to use this feature")
+  } else {
+    urlDatabase[req.params.id] = {
+      longURL: req.body.newURL,
+      userID: users[req.cookies.user_id].id
+    };
+  }
   res.redirect('/urls');
 });
 
